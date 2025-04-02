19295736c1f9541d2f526987fc2a25f36c64767d
Jedi602 <165653091+Jedi602@users.noreply.github.com>
2025-04-02 18:27:24 -0500

    Update dd_content.py

--- dd_content.py
+++ dd_content.py
@@ -1,5 +1,11 @@
-def get_random_quote():
-    pass
+def get_random_quote(quotes_file='quotes.csv'):
+    try:
+        with open(quotes_file) as csvfile:
+                quotes = [{'author':line[0],
+                           'quote': line[1] for line in csv.reader(csvfile, delimiter ='|')]
+    except Exception as e:
+        quotes = [{'author':'Eric Idle',
+                   'quote':'Always look on the bright side of life.'}]
 
 def get_weather_forecast():
     pass
